fc0d14a2439a23afb6fa2c536cebaa3ffb555446
IonConstantin <translucid900@yahoo.com>
2023-04-18 16:18:00 +0200

    18.04

--- src/components/InfoCard.jsx
+++ src/components/InfoCard.jsx
@@ -4,7 +4,7 @@ const InfoCard = () => {
   return (
     <div className='info-card' style={{ color: "white" }}>
       <h2>Ion Constantin Matache</h2>
-      <p>Hello everyone. I am a young junior developer......</p>
+      <p>Hello everyone. I am a  junior developer......</p>
     </div>
   );
 };
